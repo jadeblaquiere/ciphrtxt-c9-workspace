eae5dca57bff2bccf9e97596839fbf83b5903434
Joseph deBlaquiere <jadeblaquiere@yahoo.com>
2016-11-16 00:20:07 +0000

    name change: ciphrtext token

--- run.sh
+++ run.sh
@@ -36,5 +36,5 @@ NAKPRIV="ca92102f1fde262153ceeeaff7f5e4e98077dfb2adfa829e69581d0115acb83c"
 
 ( cd ~/workspace/msgstore ; python3 ./app.py --rpcuser=$RPCUSERNAME --rpcpass=$RPCPASSWORD --exthost=$C9_HOSTNAME --extport=80 --listenport=$PORT --nakpriv=$NAKPRIV & )
 sleep 5
-~/workspace/bin/ctcd --nodnsseed --addpeer indigo.bounceme.net --txindex --rpcuser=$RPCUSERNAME --rpcpass=$RPCPASSWORD --headercacheport=$PORT --miningaddr $MININGPUBKEY &
+~/workspace/bin/cttd --nodnsseed --addpeer indigo.bounceme.net --txindex --rpcuser=$RPCUSERNAME --rpcpass=$RPCPASSWORD --headercacheport=$PORT --miningaddr $MININGPUBKEY &
 wait
