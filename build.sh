1ca68de33a6a13902ebf3a26806e6c0388791fee
Joseph deBlaquiere <jadeblaquiere@yahoo.com>
2018-09-27 15:10:27 +0000

    build ciphrtxt client libraries for go

--- build.sh
+++ build.sh
@@ -26,6 +26,139 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# setup.sh from ctclient
+
+case "$OSTYPE" in
+  linux*)
+    LINUX_FLAVOR=`cat /etc/issue | cut -d " " -f 1 | head -1`
+    if [ "$LINUX_FLAVOR" = "Ubuntu" ]
+    then
+        echo "Configuring build for Ubuntu"
+        sudo apt-get update
+        sudo apt-get -y install check
+        sudo apt-get -y install python3-pip 
+        # the following are needed for building examples (core library only depends on GMP)
+        #####
+        # alternatively git clone git@gitlab.com:gnutls/libtasn1.git
+        sudo apt-get -y install libtasn1-6-dev libtasn1-bin
+        # alternatively brew install popt
+        sudo apt-get -y install libpopt-dev
+        # alternatively https://github.com/transmission/libb64.git
+        sudo apt-get -y install libb64-dev
+    else
+        echo "Unimplemented Linux Variant"
+        exit 1
+    fi
+    ;;
+  darwin*)
+    echo "Configuring build for Mac OSX"
+    brew update
+    brew install check
+    brew install libtasn1
+    brew install popt
+    brew install python3
+    brew upgrade python3
+    mkdir libb64-build
+    cd libb64-build
+    git clone https://github.com/transmission/libb64.git
+    cd libb64
+    make clean
+    make
+    sudo mkdir -p -m 755 /usr/local/include/b64
+    sudo install -m 644 -o root -g admin include/b64/cdecode.h /usr/local/include/b64/cdecode.h
+    sudo install -m 644 -o root -g admin include/b64/cencode.h /usr/local/include/b64/cencode.h
+    sudo install -m 644 -o root -g admin include/b64/decode.h /usr/local/include/b64/decode.h
+    sudo install -m 644 -o root -g admin include/b64/encode.h /usr/local/include/b64/encode.h
+    sudo install -m 644 -o root -g admin src/libb64.a /usr/local/lib/libb64.a
+    cd ../..
+    ;;
+  *)
+    echo "Unknown OSTYPE = $OSTYPE"
+    exit 1
+    ;;
+esac
+
+# need to build libsodium from source - expect v1.0.16
+# alternatively https://github.com/jedisct1/libsodium.git
+mkdir sodium-build
+cd sodium-build
+wget http://archive.ubuntu.com/ubuntu/pool/main/libs/libsodium/libsodium_1.0.16.orig.tar.gz
+tar xvf libsodium_1.0.16.orig.tar.gz
+cd libsodium-1.0.16
+autoreconf --install
+case "$OSTYPE" in
+  linux*)
+    ./configure --prefix=/usr
+    ;;
+  darwin*)
+    ./configure
+    ;;
+esac
+make
+sudo make install
+cd ../..
+#####
+
+git clone https://github.com/jadeblaquiere/ecclib.git
+cd ecclib
+autoreconf --install
+case "$OSTYPE" in
+  linux*)
+    ./configure --prefix=/usr
+    ;;
+  darwin*)
+    ./configure
+    ;;
+esac
+make
+sudo make install
+cd ..
+
+git clone https://github.com/jadeblaquiere/pbc.git
+cd pbc
+autoreconf --install
+case "$OSTYPE" in
+  linux*)
+    ./configure --prefix=/usr --enable-safe-clean
+    ;;
+  darwin*)
+    ./configure --enable-safe-clean
+    ;;
+esac
+make
+sudo make install
+cd ..
+
+git clone https://github.com/jadeblaquiere/fspke.git
+cd fspke
+autoreconf --install
+case "$OSTYPE" in
+  linux*)
+    ./configure --prefix=/usr
+    ;;
+  darwin*)
+    ./configure
+    ;;
+esac
+make
+sudo make install
+cd ..
+
+git clone https://github.com/jadeblaquiere/ctclient.git
+cd ctclient
+autoreconf --install
+case "$OSTYPE" in
+  linux*)
+    ./configure --prefix=/usr
+    ;;
+  darwin*)
+    ./configure
+    ;;
+esac
+make
+sudo make install
+cd ..
+
 # need golang 1.10 for cttd
 sudo add-apt-repository -y ppa:jonathonf/golang
 sudo apt-get update
@@ -38,6 +171,7 @@ echo "export PATH=/usr/lib/go-1.10/bin:\$PATH" >> ~/.profile
 export GOROOT=/usr/lib/go-1.10
 echo "export GOROOT=/usr/lib/go-1.10" >> ~/.profile
 
+go get github.com/jadeblaquiere/ctclient/ctgo
 go get github.com/jadeblaquiere/cttd
 
 # need protoc v3 and protoc-gen-go for cttwallet
